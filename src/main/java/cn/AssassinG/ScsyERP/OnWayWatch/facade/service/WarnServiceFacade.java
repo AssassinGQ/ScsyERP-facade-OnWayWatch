f7fea9a5b476c35802de29fc5637971e66c774f4
AssassinG <hangq_zju@hotmail.com>
2018-12-14 21:47:28 +0800

    给异常信息服务添加了添加图片和删除图片的接口及其实现

--- src/main/java/cn/AssassinG/ScsyERP/OnWayWatch/facade/service/WarnServiceFacade.java
+++ src/main/java/cn/AssassinG/ScsyERP/OnWayWatch/facade/service/WarnServiceFacade.java
@@ -4,4 +4,6 @@ import cn.AssassinG.ScsyERP.common.core.service.BaseService;
 import cn.AssassinG.ScsyERP.OnWayWatch.facade.entity.Warn;
 
 public interface WarnServiceFacade extends BaseService<Warn> {
+    void addPicture(Long entityId, Long pictureId);
+    void removePicture(Long entityId, Long pictureId);
 }
